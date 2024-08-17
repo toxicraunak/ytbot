ee1f1c9b6d500231c28b98c057e21654d0c606ab
Tanjiro Kamado <120044797+toxicraunak@users.noreply.github.com>
2024-08-17 19:15:41 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -2,30 +2,30 @@ import os
 
 class Config:
 
-    BOT_TOKEN = ""
+    BOT_TOKEN = "7461482650:AAF0dme5l8NQX4W0wHXk172o29JqBnTVE0I"
 
-    SESSION_NAME = ""
+    SESSION_NAME = "mikasa_yt_bot"
 
-    API_ID = ""
+    API_ID = "11779786"
 
-    API_HASH = ""
+    API_HASH = "d7ef58ca40133227783348fd3c0923c4"
 
-    CLIENT_ID = ""
+    CLIENT_ID = "38589946403-8tccpolvf4fvmgi7idlngiuo39pf65ag.apps.googleusercontent.com"
 
-    CLIENT_SECRET = ""
+    CLIENT_SECRET = "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"
 
-    AUTH_USERS = [942731625]
+    AUTH_USERS = [5195444280,6610511001,6876898157]
 
     VIDEO_DESCRIPTION = ""
 
     VIDEO_CATEGORY = ""
 
-    VIDEO_TITLE_PREFIX = ""
+    VIDEO_TITLE_PREFIX = "Instagram viral video"
 
     VIDEO_TITLE_SUFFIX = ""
     
     DEBUG = bool()
 
-    UPLOAD_MODE = "unlisted"
+    UPLOAD_MODE = "public"
     
     CRED_FILE = "auth_token.txt"
